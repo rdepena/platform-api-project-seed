8726e74767c8dab8d903d836bae02e9d72294b3c
Ricardo Depena <ricardo@isnull.co>
2021-09-21 19:43:02 -0400

    for smallhands

--- js/platform-window.js
+++ js/platform-window.js
@@ -5,9 +5,15 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 const myLayoutContainer = document.getElementById(CONTAINER_ID);
-myLayoutContainer.addEventListener('tab-created', function(event) {
-    const { tabSelector } = event.detail;
-    const tabElement = document.getElementById(tabSelector);
-    const titleElement = tabElement.getElementsByClassName("lm_title")[0];
-    titleElement.className += "sparks dotline-extrathick";
+myLayoutContainer.addEventListener('tab-created', async (event) => {
+    const { tabSelector, uuid, name } = event.detail;
+    const targetView = fin.View.wrapSync({ uuid, name });
+    const viewDetails = await targetView.getOptions();
+   
+    //restrict this logic to openfin views
+    if (viewDetails.url.search("openfin")) {
+        const tabElement = document.getElementById(tabSelector);
+        const titleElement = tabElement.getElementsByClassName("lm_title")[0];
+        titleElement.className += " sparks dotline-extrathick";
+    }
 });
--- js/sparkline-preload.js
+++ js/sparkline-preload.js
@@ -1,19 +1,37 @@
 (async () => {
-    function rand() {
-        return Math.floor(Math.random() * 100);
+    const tickerNames = [
+        "APPL",
+        "TSLA",
+        "IBM",
+        "MSFT"
+    ];
+
+    const currentTicker = tickerNames[rand(4)];
+    const data = [];
+
+    //We seed the data
+    while(data.length < 13) {
+        data.push(rand(100));
     }
 
+    function rand(ciel) {
+        return Math.floor(Math.random() * ciel);
+    }
+    
     function generateSparkTitle() {
-        document.title = `Appl:{${rand()},${rand()},${rand()},${rand()},${rand()},${rand()},${rand()},${rand()}}`;
+        document.title = `${currentTicker}: ${data.join(",")}`;
+        data.push(rand(100));
+        data.shift();
     }
     
     function loopIt() {
         generateSparkTitle();
         setTimeout(loopIt, 1000);
     }
+
+    
     //only do this for our demos.
     if (document.location.origin === "https://cdn.openfin.co") {
         loopIt();
     }
-    
 })();
